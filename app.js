8a9d7f5ed4c2484aa27c4de720f639eeeb777f2f
atharv0811 <karnekaratharv12@gmail.com>
2023-09-24 21:19:12 +0530

    added url changing html page functionality

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("Atharv")
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title>WebPage</title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/about') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title>WebPage</title></head>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/node') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title>WebPage</title></head>')
+        res.write('<body><h1>Welcome to Node JS Projects</h1></body>')
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 server.listen(4000);
