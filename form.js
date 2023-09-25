dfe79f33531383090428ec6a12d741d5a247e69c
atharv0811 <karnekaratharv12@gmail.com>
2023-09-25 20:28:41 +0530

    showing input on the web page

--- form.js
+++ form.js
@@ -1,33 +1,68 @@
 const http = require('http');
 const fs = require('fs');
-
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<html>');
-        res.write('<head><title>WebPage</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
+    if (req.url === "/home") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>My first page</title></head>");
+        res.write("<body><h1>Welcome home</h1></body>");
+        res.write("</html>");
+        res.end();
+    } else if (req.url === "/about") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>My first page</title></head>");
+        res.write("<body><h1>Welcome to About Us page</h1></body>");
+        res.write("</html>");
+        res.end();
+    } else if (req.url === "/node") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>My first page</title></head>");
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+        res.write("</html>");
+        res.end();
+    } else if (req.url === "/") {
+
+        fs.readFile("message.txt", (err, data) => {
+            if (err) {
+                res.setHeader("Content-Type", "text/html");
+                res.write("<html>");
+                res.write("<head><title>My first page</title></head>");
+                res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+                res.write("</html>");
+                res.end();
+            } else {
+                res.setHeader("Content-Type", "text/html");
+                res.write("<html>");
+                res.write("<head><title>My first page</title></head>");
+                res.write(`<body><form action="/message" method="POST"><div><h1>${data}</h1></div><input type="text" name="message"><button type="submit">Send</button></form></body>`);
+                res.write("</html>");
+                res.end();
+            }
+        });
+
+    } else if (req.url === "/message" && req.method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
-            body.push(chunk);
+            body.push(chunk)
         });
-
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
+            const message = parsedBody.split("=").splice(1, 1).toString();
+            fs.writeFile('message.txt', message, () => {
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
-                return res.end();
+                res.end();
             });
-        });
-
+        })
+    } else {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>My first page</title></head>");
+        res.write("<body><h1>Page not found</h1></body>");
+        res.write("</html>");
+        res.end();
     }
 });
 
